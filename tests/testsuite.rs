dc82e4baa0405f40713225cc2a4bc57162c94bba
Aakash Patel <aakpat6@gmail.com>
2020-05-02 23:19:05 -0700

    Add LIT tests

---
+++ tests/testsuite.rs
@@ -0,0 +1,34 @@
+extern crate lit;
+
+use std::env;
+use std::path::PathBuf;
+
+fn bin_dir() -> PathBuf {
+    env::current_exe()
+        .ok()
+        .map(|mut path| {
+            path.pop();
+            path.pop();
+            path
+        })
+        .unwrap()
+}
+
+fn bin_path() -> String {
+    bin_dir()
+        .join(format!("lox{}", env::consts::EXE_SUFFIX))
+        .to_str()
+        .unwrap()
+        .to_string()
+}
+
+#[test]
+fn run() {
+    lit::run::tests(|config| {
+        config.add_search_path("tests/lox/");
+        config.add_extension("lox");
+        config
+            .constants
+            .insert("lox".to_owned(), bin_path());
+    }).expect("LIT tests failed");
+}
