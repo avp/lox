be7c698861def364349241989d755714e454050f
Aakash Patel <aakpat6@gmail.com>
2020-11-03 20:12:23 -0800

    support

---
+++ src/support.rs
@@ -0,0 +1,12 @@
+use std::mem::transmute;
+
+pub unsafe fn slice_get_2_mut<T>(
+    t: &mut [T],
+    i1: usize,
+    i2: usize,
+) -> (&mut T, &mut T) {
+    debug_assert!(i1 != i2, "Can't reference same index twice");
+    let ref1 = transmute(transmute::<&mut T, *mut T>(&mut t[i1]));
+    let ref2 = transmute(transmute::<&mut T, *mut T>(&mut t[i2]));
+    (ref1, ref2)
+}
